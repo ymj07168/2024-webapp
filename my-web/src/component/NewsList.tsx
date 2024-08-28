443e274d62788ae0a583793fcdff9f3c64e72c72
이효원 <ymj07168@gmail.com>
2024-08-28 17:38:36 +0900

    swiper 추가

--- my-web/src/component/NewsList.tsx
+++ my-web/src/component/NewsList.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import NewsCard from "./NewsCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
 
 const ListContainer = styled.div`
   max-width: 1360px;
@@ -14,19 +18,43 @@ const ListContainer = styled.div`
     gap: 10px;
     overflow-x: scroll;
   }
+  .mySwiper {
+    width: 100%;
+  }
 `;
 const NewsList = () => {
   return (
     <ListContainer>
       <h1>재단소식</h1>
-      <div className="news-list">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-      </div>
+      <Swiper
+        className="mySwiper"
+        slidesPerView={3} // 한 번에 3개의 슬라이드를 표시
+        scrollbar={{
+          hide: true,
+        }}
+      >
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <NewsCard />
+        </SwiperSlide>
+      </Swiper>
     </ListContainer>
   );
 };
