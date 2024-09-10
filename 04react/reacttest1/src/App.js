82315ddb265d5e5b85fc931f913df919de6ed440
이효원 <ymj07168@gmail.com>
2024-09-10 15:25:19 +0900

    react test

---
+++ 04react/reacttest1/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState([
+    "1. hihihihi",
+    "1. hihihihi",
+    "1. hihihihi",
+    "1. hihihihi",
+  ]);
+
+  const [num, setNum] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  const listView = () => {
+    alert("ddd");
+  };
+
+  return <div>
+    {data.map((item, i) => {
+      return (
+        
+      )
+    })}
+  </div>;
+};
+
+export default App;
