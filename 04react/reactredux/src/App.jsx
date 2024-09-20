db6ecd870ddb198000163724816664182ba1c2ab
이효원 <ymj07168@gmail.com>
2024-09-20 09:03:18 +0900

    feat: reducer

--- 04react/reactredux/src/App.jsx
+++ 04react/reactredux/src/App.jsx
@@ -1,14 +1,56 @@
+import React from "react";
 import { Provider } from "react-redux";
-import store from "./store";
-import Counter from "./components/Counter.jsx";
+import { createStore } from "redux";
 
-function App() {
+const initData = {
+  cart: [],
+};
+
+const reducer = (state = initData, action) => {
+  switch (action.type) {
+    case "add_cart":
+      return [...state.cart, action.payload];
+    
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const App = () => {
   return (
     <Provider store={store}>
-      <div>App</div>
-      <Counter />
+      <div>shop</div>
+      <ProductionList />
+      <Cart />
     </Provider>
   );
-}
+};
 
+const products = [
+  { id: 1, name: "product1111" },
+  { id: 1, name: "product1111" },
+  { id: 1, name: "product1111" },
+  { id: 1, name: "product1111" },
+];
+
+const ProductionList = () => {
+  return (
+    <>
+      <div>product</div>
+      {products.map((item, i) => {
+        return <div key={i}>{item}</div>;
+      })}
+    </>
+  );
+};
+
+const Cart = () => {
+  return (
+    <>
+      <div>cart</div>
+    </>
+  );
+};
 export default App;
